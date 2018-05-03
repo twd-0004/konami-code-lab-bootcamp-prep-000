10ad113204f54150ab3e5972d99693102d0da593
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 15:26:21 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-  const body = document.getElementByTagName('body')
+  const body = document.getElementsByTagName('body')
   body.addEventListener('keydown',onKeyDownHandler(e))
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
