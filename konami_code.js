040700b162769daf225a3b2cb24b950ad2be84c0
Tyler Dreher <drehertyler@gmail.com>
2018-05-03 15:32:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,7 +3,7 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-  const body = document.getElementsByTagName('body')
+  const body = document.querySelector('body')
   console.log(body)
   body.addEventListener('keydown',function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
@@ -34,5 +34,5 @@ function init() {
       index = 0;
     }
   })
-    
+
 }
